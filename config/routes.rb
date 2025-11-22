c7e2f6fec9801adbaeb9ee66ea760d8e290448d8
Goto-star <goyu620@gmail.com>
2025-11-22 17:05:44 +0900

    add: 言い訳リストの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "excuse_requests#new"
-  resources :excuse_requests, only: [:new, :create, :show, :index]
+  resources :excuse_requests, only: [:new, :create, :show, :index, :destroy]
 
 end
